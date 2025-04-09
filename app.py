148eff44dc90267cb6cd868355867056105bb2be
VinayReddy4322 <166357424+VinayReddy4322@users.noreply.github.com>
2025-04-09 11:14:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from flask import Flask, request
+from youtube_transcript_api import YouTubeTranscriptApi
+from transformers import pipeline
+
+app = Flask(__name__)
+
+@app.get('/summary')
+def summary_api():
+    url = request.args.get('url', '')
+    video_id = url.split('=')[1]
+    print(video_id)
+    summary = get_summary(get_transcript(video_id))
+    return summary, 200
+
+def get_transcript(video_id):
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id,)
+    transcript = ' '.join([d['text'] for d in transcript_list])
+    print(transcript)
+    return transcript
+
+def get_summary(transcript):
+    summariser = pipeline('summarization')
+    summary = ''
+    for i in range(0, (len(transcript)//1000)+1):
+        summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
+        summary = summary + summary_text + ' '
+    print(summary)
+    return summary
+    
+
+if __name__ == '__main__':
+    app.run()
